6ab370733a71d880f0432fe9ddb4db616f200c1c
Robin Huerta Tarazona <robinhuerta@gmail.com>
2026-03-26 00:00:35 -0500

    UI Pixel-Perfect: Top 10 numbers y MiniModal refinado

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -11,17 +11,17 @@ const MovieCard = ({ movie, isTop10, onPlay, isPortrait }) => {
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
+      {isTop10 && (
+        <div className="movie-card__rank">
+          {movie.rank}
+        </div>
+      )}
       <div className="movie-card__clickable" onClick={() => onPlay && onPlay(movie)}>
-        {isTop10 && (
-          <div className="movie-card__rank">
-            {movie.rank}
-          </div>
-        )}
         <img className="movie-card__image" src={movie.image} alt={movie.title} />
         {movie.badge && <span className="movie-card__badge">{movie.badge}</span>}
       </div>
 
-      {isHovered && <MiniModal movie={movie} onPlay={onPlay} isPortrait={isPortrait} />}
+      {isHovered && <MiniModal movie={movie} onPlay={onPlay} isPortrait={isPortrait || isTop10} />}
     </div>
   );
 };
